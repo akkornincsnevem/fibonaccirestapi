e58c2a7230a43ae640a85b4d0f1e93acb3fe8cc6
hallgato <hallgato@hallgato.hu>
2025-03-10 12:27:19 +0100

    kb minden feladat

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -1,11 +1,25 @@
 package hu.obuda.devops.fibonaccirestapi.service
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        if(n > 0){
+            if (n <= 1) return n
+            if( n > 46) throw ResponseStatusException(HttpStatus.BAD_REQUEST, "n > 46 is not allowed")
+            var a = 0
+            var b = 1
+            for (i in 2..n) {
+                val temp = a + b
+                a = b
+                b = temp
+            }
+            return b
+        }
+        else
+            throw IllegalArgumentException("n must be greater than 0")
     }
 }
--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -1,8 +1,12 @@
 package hu.obuda.devops.fibonaccirestapi.service
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FibonacciServiceTest {
@@ -17,6 +21,67 @@ class FibonacciServiceTest {
         // then
         Assertions.assertEquals(1, result)
     }
+    @Test
+    fun shouldReturn1WhenCall1() {
+        // given
 
+        // when
+        val result: Int = underTest.fibonacci(2)
+        // then
+        Assertions.assertEquals(1, result)
+    }
+
+    @Test
+    fun shouldReturn55WhenCall10() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(2)
+        // then
+        Assertions.assertEquals(1, result)
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenCallFibonacciWithNegativeInput() {
+        // given
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            underTest.fibonacci(-1)
+        }
+
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            underTest.fibonacci(-1)
+        }
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenCallFibonacciWith0Input() {
+        // given
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            underTest.fibonacci(0)
+        }
+
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            underTest.fibonacci(0)
+        }
+    }
+
+    @Test
+    fun `should throw BAD_REQUEST for n = 47`() {
+        // Given
+        val n = 47
+
+        // When & Then
+        val exception = assertThrows(ResponseStatusException::class.java) {
+            underTest.fibonacci(n)
+        }
+
+        // Assert that the exception has the correct status code and message
+        assertEquals(HttpStatus.BAD_REQUEST, exception.status)
+        assertEquals("n > 46 is not allowed", exception.reason)
+    }
     // TODO - Test with greater numbers and test edge cases
 }
